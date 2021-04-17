03b4f4736f88d4faf0cbc83bed111799d8a75684
hungoke <nvh66725@gmail.com>
2021-04-17 17:51:43 +0700

    commit

--- src/components/header/NavigationDesktop.js
+++ src/components/header/NavigationDesktop.js
@@ -1,43 +1,39 @@
 import React, { useEffect } from "react"
-import { Row, Col } from "antd"
 import { Link } from "react-router-dom"
 import { ShoppingCartOutlined } from "@material-ui/icons"
 import { navigations } from "../../router/navigations"
-
-const NavBarDesktop = ({ routes, setIsShowMenu }) => {
+import "../../assets/scss/NavigationDesktop.scss"
+const NavBarDesktop = ({ setIsShowMenu }) => {
   useEffect(() => {
     setIsShowMenu(false)
   })
 
   return (
-    <>
-      <Row>
-        <Col span={3}>
-          <Link to="/">
-            <span className="navBar__nav__logo">Nông sản sạch</span>
+    <div className="navbar">
+      <div className="navbar__logo">
+        <Link to="/">
+          <span>Nông sản sạch</span>
+        </Link>
+      </div>
+
+      <div className="navbar__search">
+        <input type="text" placeholder="Tìm kiếm" />
+      </div>
+
+      {navigations.map((item, index) => (
+        <div className="navbar__item" key={index}>
+          <Link to={item.to}>
+            <span>{item.lable}</span>
           </Link>
-        </Col>
+        </div>
+      ))}
 
-        <Col span={4}>
-          <input type="text" />
-        </Col>
-
-        {navigations.map((item, index) => (
-          <Col key={index} span={2}>
-            <Link to={item.to}>
-              <span className="navBar__nav__link">{item.lable}</span>
-            </Link>
-          </Col>
-        ))}
-
-        <Col span={3}>
-          <Link style={{ display: "flex", flexDirection: "row" }}>
-            <ShoppingCartOutlined style={{ color: "#9ede73" }} />
-            <span style={{ color: "#d6d6d6" }}>Giỏ hàng</span>
-          </Link>
-        </Col>
-      </Row>
-    </>
+      <div className="navbar__cart">
+        <Link style={{ display: "flex", alignContent: "center" }}>
+          <ShoppingCartOutlined style={{ color: "#9ede73" }} />
+        </Link>
+      </div>
+    </div>
   )
 }
 
