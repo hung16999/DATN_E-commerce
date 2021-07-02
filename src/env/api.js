c6a852aeeb17e035030bc577cc4ed9215b9d2b16
hung nguyen van <nvh66725@gmail.com>
2021-07-02 22:30:37 +0700

    ket thuc

--- src/env/api.js
+++ src/env/api.js
@@ -3,5 +3,5 @@ import axios from "axios"
 const ipConfig = `192.168.1.49`
 
 export default axios.create({
-  baseURL: `http://${ipConfig}/nong_san_sach/`,
+  baseURL: `http://${ipConfig}/backend_nongsansach/`,
 })
